bc7f29879255728193dd8aa6b08b38354a00af43
d0kt0r0 <ogbornd@mcmaster.ca>
2018-08-18 09:10:41 -0400

    parser in browser skeleton

--- Main.hs
+++ Main.hs
@@ -1,7 +1,23 @@
 module Main where
 
 import Reflex.Dom
+import Sound.Punctual.Types
 import Sound.Punctual.Evaluation
 
 main :: IO ()
-main = mainWidget $ text "Punctual"
+main = mainWidget $ do
+  el "div" $ text "Punctual"
+  evals <- el "div" $ textAreaWithEvalButton
+  status <- punctual evals
+  dynText status
+
+textAreaWithEvalButton :: MonadWidget t m => m (Event t String)
+textAreaWithEvalButton = do
+  y <- el "div" $ button "eval"
+  x <- el "div" $ textArea def
+  return $ tagDyn (_textArea_value x) y
+
+punctual :: MonadWidget t m => Event t String -> m (Dynamic t String)
+punctual evals = do
+  let x = fmap runPunctualParser evals
+  holdDyn "" $ fmap show x
