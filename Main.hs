7e71ff075d170f3d99090a512ef59dafb04ad531
d0kt0r0 <ogbornd@mcmaster.ca>
2018-12-31 11:10:30 -0500

    updated to most recent MusicW (providing audio context instead of using global one)

--- Main.hs
+++ Main.hs
@@ -11,24 +11,27 @@ import Sound.Punctual.Types
 import Sound.Punctual.Evaluation
 import Sound.Punctual.Parser
 import Sound.Punctual.PunctualW
+import Sound.MusicW
+import Sound.MusicW.AudioRoutingGraph
 
 main :: IO ()
 main = mainWidget $ do
+  ac <- liftIO $ getAudioContext
   el "div" $ text "Punctual"
   evalButton <- el "div" $ button "eval"
   code <- el "div" $ textArea def
   let evaled = tagDyn (_textArea_value code) evalButton
   let parsed = fmap (runPunctualParser . unpack) evaled
-  punctualReflex $ fmapMaybe (either (const Nothing) Just) parsed
+  punctualReflex ac $ fmapMaybe (either (const Nothing) Just) parsed
   let errors = fmapMaybe (either (Just . show) (Just . show)) parsed
   status <- holdDyn "" $ fmap (pack . show) errors
   dynText status
 
-punctualReflex :: MonadWidget t m => Event t [Expression] -> m ()
-punctualReflex exprs = mdo
-  now <- liftIO $ getCurrentTime
+punctualReflex :: MonadWidget t m => WebAudioContext -> Event t [Expression] -> m ()
+punctualReflex ac exprs = mdo
+  now <- liftIO $ getAudioTime ac
   evals <- performEvent $ fmap (liftIO . evaluationNow) exprs
   let punctualWAndEval = attachDyn currentPunctualW evals
   newPunctualW <- performEvent $ fmap (liftIO . (uncurry updatePunctualW)) punctualWAndEval
-  currentPunctualW <- holdDyn (emptyPunctualW now)  newPunctualW
+  currentPunctualW <- holdDyn (emptyPunctualW ac now)  newPunctualW
   return ()
