2f4f007b6caf02cde67e2907d159dcb5d1ef4de2
d0kt0r0 <ogbornd@mcmaster.ca>
2020-01-26 10:44:06 -0500

    rgbhsv working post-refactor

--- Sound/Punctual/FragmentShader.hs
+++ Sound/Punctual/FragmentShader.hs
@@ -239,6 +239,13 @@ header
    \  vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);\
    \  vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);\
    \  return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);}\
+   \vec3 rgbhsv(vec3 c){\
+   \  vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0); \
+   \  vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));\
+   \  vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));\
+   \  float d = q.x - min(q.w, q.y);\
+   \  float e = 1.0e-10;\
+   \  return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);}\
    \float vline(float x,float w) { if(abs(fx()-x)<w) return 1.; else return 0.;}\
    \float hline(float y,float w) { if(abs(fy()-y)<w) return 1.; else return 0.;}\
    \float iline(vec2 xy1,vec2 xy2,float w) {\
@@ -271,7 +278,7 @@ header
    \ return between(vec2(x1,x2),fx())*between(vec2(y1,y2),fy());}\
    \float circle(vec2 xy,float r) { if(distance(xy,fxy())<r)return 1.; else return 0.;}\
    \float point(vec2 xy) { return circle(xy,0.002); }"
-   -- thanks to http://lolengine.net/blog/2013/07/27/rgb-to-hsv-in-glsl for the hsv to rgb algorithm above!
+   -- thanks to http://lolengine.net/blog/2013/07/27/rgb-to-hsv-in-glsl for the HSV-RGB conversion algorithms above!
 
 isVec3 :: Action -> Bool
 isVec3 x = elem RGB (outputs x) || elem HSV (outputs x)
