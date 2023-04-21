5240e49f14c2f2e412266df792c316fea066a768
David Ogborn <ogbornd@mcmaster.ca>
2023-04-21 14:07:04 -0400

    update (webcam always on)

--- lib.js
+++ lib.js
@@ -3027,10 +3027,24 @@ goog.crypt.Md5.prototype.digest = function() {
 };
 function h$base_access(file, file_off, mode, c) {
     ;
+    if(h$isNode) {
+        h$fs.stat(fd, function(err, fs) {
+            if(err) {
+                h$handleErrnoC(err, -1, 0, c);
+            } else {
+                c(mode & fs.mode);
+            }
+        });
+    } else
         h$unsupported(-1, c);
 }
 function h$base_chmod(file, file_off, mode, c) {
     ;
+    if(h$isNode) {
+        h$fs.chmod(h$decodeUtf8z(file, file_off), mode, function(err) {
+            h$handleErrnoC(err, -1, 0, c);
+        });
+    } else
         h$unsupported(-1, c);
 }
 function h$base_close(fd, c) {
@@ -3082,6 +3096,16 @@ function h$base_dup2(fd, new_fd, c) {
 }
 function h$base_fstat(fd, stat, stat_off, c) {
     ;
+    if(h$isNode) {
+        h$fs.fstat(fd, function(err, fs) {
+            if(err) {
+                h$handlErrnoC(err, -1, 0, c);
+            } else {
+                h$base_fillStat(fs, stat, stat_off);
+                c(0);
+            }
+        });
+    } else
         h$unsupported(-1, c);
 }
 function h$base_isatty(fd) {
@@ -3095,14 +3119,102 @@ function h$base_isatty(fd) {
 }
 function h$base_lseek(fd, pos_1, pos_2, whence, c) {
     ;
+    if(h$isNode) {
+        var p = goog.math.Long.fromBits(pos_2, pos_1), p1;
+        var o = h$base_fds[fd];
+        if(!o) {
+            h$errno = CONST_BADF;
+            c(-1,-1);
+        } else {
+            switch(whence) {
+            case 0:
+                o.pos = p.toNumber();
+                c(p.getHighBits(), p.getLowBits());
+                break;
+            case 1:
+                o.pos += p.toNumber();
+                p1 = goog.math.Long.fromNumber(o.pos);
+                c(p1.getHighBits(), p1.getLowBits());
+                break;
+            case 2:
+                h$fs.fstat(fd, function(err, fs) {
+                    if(err) {
+                        h$setErrno(err);
+                        c(-1,-1);
+                    } else {
+                        o.pos = fs.size + p.toNumber();
+                        p1 = goog.math.Long.fromNumber(o.pos);
+                        c(p1.getHighBits(), p1.getLowBits());
+                    }
+                });
+                break;
+            default:
+                h$errno = 22;
+                c(-1,-1);
+            }
+        }
+    } else {
         h$unsupported();
         c(-1, -1);
+    }
 }
 function h$base_lstat(file, file_off, stat, stat_off, c) {
     ;
+    if(h$isNode) {
+        h$fs.lstat(h$decodeUtf8z(file, file_off), function(err, fs) {
+            if(err) {
+                h$handleErrnoC(err, -1, 0, c);
+            } else {
+                h$base_fillStat(fs, stat, stat_off);
+                c(0);
+            }
+        });
+    } else
         h$unsupported(-1, c);
 }
 function h$base_open(file, file_off, how, mode, c) {
+    if(h$isNode) {
+        var flags, off;
+        var fp = h$decodeUtf8z(file, file_off);
+        ;
+        var acc = how & h$base_o_accmode;
+        if(acc === h$base_o_rdonly) {
+            flags = h$processConstants['fs']['O_RDONLY'];
+        } else if(acc === h$base_o_wronly) {
+            flags = h$processConstants['fs']['O_WRONLY'];
+        } else {
+            flags = h$processConstants['fs']['O_RDWR'];
+        }
+        off = (how & h$base_o_append) ? -1 : 0;
+        flags = flags | ((how & h$base_o_trunc) ? h$processConstants['fs']['O_TRUNC'] : 0)
+                      | ((how & h$base_o_creat) ? h$processConstants['fs']['O_CREAT'] : 0)
+                      | ((how & h$base_o_excl) ? h$processConstants['fs']['O_EXCL'] : 0)
+                      | ((how & h$base_o_append) ? h$processConstants['fs']['O_APPEND'] : 0);
+        h$fs.open(fp, flags, mode, function(err, fd) {
+            if(err) {
+                h$handleErrnoC(err, -1, 0, c);
+            } else {
+                var f = function(p) {
+                    h$base_fds[fd] = { read: h$base_readFile
+                                     , write: h$base_writeFile
+                                     , close: h$base_closeFile
+                                     , fd: fd
+                                     , pos: p
+                                     , refs: 1
+                                     };
+                    ;
+                    c(fd);
+                }
+                if(off === -1) {
+                    h$fs.stat(fp, function(err, fs) {
+                        if(err) h$handleErrnoC(err, -1, 0, c); else f(fs.size);
+                    });
+                } else {
+                    f(0);
+                }
+            }
+        });
+    } else
         h$unsupported(-1, c);
 }
 function h$base_read(fd, buf, buf_off, n, c) {
@@ -3118,10 +3230,21 @@ function h$base_read(fd, buf, buf_off, n, c) {
 }
 function h$base_stat(file, file_off, stat, stat_off, c) {
     ;
+    if(h$isNode) {
+        h$fs.stat(h$decodeUtf8z(file, file_off), function(err, fs) {
+            if(err) {
+                h$handlErrnoC(err, -1, 0, c);
+            } else {
+                h$base_fillStat(fs, stat, stat_off);
+                c(0);
+            }
+        });
+    } else
         h$unsupported(-1, c);
 }
 function h$base_umask(mode) {
     ;
+    if(h$isNode) return process.umask(mode);
     return 0;
 }
 function h$base_write(fd, buf, buf_off, n, c) {
@@ -3137,18 +3260,34 @@ function h$base_write(fd, buf, buf_off, n, c) {
 }
 function h$base_ftruncate(fd, pos_1, pos_2, c) {
     ;
+    if(h$isNode) {
+        h$fs.ftruncate(fd, goog.math.Long.fromBits(pos_2, pos_1).toNumber(), function(err) {
+            h$handleErrnoC(err, -1, 0, c);
+        });
+    } else
         h$unsupported(-1, c);
 }
 function h$base_unlink(file, file_off, c) {
     ;
+    if(h$isNode) {
+        h$fs.unlink(h$decodeUtf8z(file, file_off), function(err) {
+            h$handleErrnoC(err, -1, 0, c);
+        });
+    } else
         h$unsupported(-1, c);
 }
 function h$base_getpid() {
     ;
+    if(h$isNode) return process.pid;
     return 0;
 }
 function h$base_link(file1, file1_off, file2, file2_off, c) {
     ;
+    if(h$isNode) {
+        h$fs.link(h$decodeUtf8z(file1, file1_off), h$decodeUtf8z(file2, file2_off), function(err) {
+            h$handleErrnoC(err, -1, 0, c);
+        });
+    } else
         h$unsupported(-1, c);
 }
 function h$base_mkfifo(file, file_off, mode, c) {
@@ -3171,6 +3310,24 @@ function h$base_tcsetattr(attr, val, termios, termios_off) {
 }
 function h$base_utime(file, file_off, timbuf, timbuf_off, c) {
     ;
+    if(h$isNode) {
+        h$fs.fstat(h$decodeUtf8z(file, file_off), function(err, fs) {
+            if(err) {
+                h$handleErrnoC(err, 0, -1, c);
+            } else {
+                var atime = goog.math.Long.fromNumber(fs.atime.getTime());
+                var mtime = goog.math.Long.fromNumber(fs.mtime.getTime());
+                var ctime = goog.math.Long.fromNumber(fs.ctime.getTime());
+                timbuf.i3[0] = atime.getHighBits();
+                timbuf.i3[1] = atime.getLowBits();
+                timbuf.i3[2] = mtime.getHighBits();
+                timbuf.i3[3] = mtime.getLowBits();
+                timbuf.i3[4] = ctime.getHighBits();
+                timbuf.i3[5] = ctime.getLowBits();
+                c(0);
+            }
+        });
+    } else
         h$unsupported(-1, c);
 }
 function h$base_waitpid(pid, stat, stat_off, options, c) {
@@ -3201,6 +3358,22 @@ function h$base_c_s_isdir(mode) {
 }
 function h$base_c_s_isfifo(mode) {
     return 0;
+}
+function h$base_fillStat(fs, b, off) {
+    if(off%4) throw "h$base_fillStat: not aligned";
+    var o = off>>2;
+    b.i3[o+0] = fs.mode;
+    var s = goog.math.Long.fromNumber(fs.size);
+    b.i3[o+1] = s.getHighBits();
+    b.i3[o+2] = s.getLowBits();
+    b.i3[o+3] = 0;
+    b.i3[o+4] = 0;
+    b.i3[o+5] = fs.dev;
+    var i = goog.math.Long.fromNumber(fs.ino);
+    b.i3[o+6] = i.getHighBits();
+    b.i3[o+7] = i.getLowBits();
+    b.i3[o+8] = fs.uid;
+    b.i3[o+9] = fs.gid;
 }
               var h$base_sizeof_stat = 40;
 function h$base_st_mtime(stat, stat_off) {
@@ -3266,6 +3439,147 @@ var h$base_readStdin , h$base_writeStderr, h$base_writeStdout;
 var h$base_isattyStdin = false, h$base_isattyStdout = false, h$base_isattyStderr = false;
 var h$base_closeStdin = null, h$base_closeStderr = null, h$base_closeStdout = null;
 var h$base_readFile, h$base_writeFile, h$base_closeFile;
+var h$base_stdin_waiting = new h$Queue();
+var h$base_stdin_chunk = { buf: null
+                           , pos: 0
+                           , processing: false
+                           };
+var h$base_stdin_eof = false;
+var h$base_process_stdin = function() {
+    var c = h$base_stdin_chunk;
+    var q = h$base_stdin_waiting;
+    if(!q.length() || c.processing) return;
+    c.processing = true;
+    if(!c.buf) { c.pos = 0; c.buf = process.stdin.read(); }
+    while(c.buf && q.length()) {
+        var x = q.dequeue();
+        var n = Math.min(c.buf.length - c.pos, x.n);
+        for(var i=0;i<n;i++) {
+            x.buf.u8[i+x.off] = c.buf[c.pos+i];
+        }
+        c.pos += n;
+        x.c(n);
+        if(c.pos >= c.buf.length) c.buf = null;
+        if(!c.buf && q.length()) { c.pos = 0; c.buf = process.stdin.read(); }
+    }
+    while(h$base_stdin_eof && q.length()) q.dequeue().c(0);
+    c.processing = false;
+}
+if(h$isNode) {
+    h$base_closeFile = function(fd, fdo, c) {
+        ;
+        var real_fd = typeof fdo.fd === 'number' ? fdo.fd : fd;
+        h$fs.close(real_fd, function(err) {
+            delete h$base_fds[fd];
+            h$handleErrnoC(err, -1, 0, c);
+        });
+    }
+    h$base_readFile = function(fd, fdo, buf, buf_offset, n, c) {
+        var pos = typeof fdo.pos === 'number' ? fdo.pos : null;
+        ;
+        var real_fd = typeof fdo.fd === 'number' ? fdo.fd : fd;
+        h$fs.read(real_fd, Buffer.alloc(n), 0, n, pos, function(err, bytesRead, nbuf) {
+            if(err) {
+                h$setErrno(err);
+                c(-1);
+            } else {
+                for(var i=bytesRead-1;i>=0;i--) buf.u8[buf_offset+i] = nbuf[i];
+                if(typeof fdo.pos === 'number') fdo.pos += bytesRead;
+                c(bytesRead);
+            }
+        });
+    }
+    h$base_readStdin = function(fd, fdo, buf, buf_offset, n, c) {
+        ;
+        h$base_stdin_waiting.enqueue({buf: buf, off: buf_offset, n: n, c: c});
+        h$base_process_stdin();
+    }
+    h$base_closeStdin = function(fd, fdo, c) {
+        ;
+        c(0);
+    }
+    h$base_writeFile = function(fd, fdo, buf, buf_offset, n, c) {
+        var pos = typeof fdo.pos === 'number' ? fdo.pos : null;
+        ;
+        var nbuf = Buffer.alloc(n);
+        for(var i=0;i<n;i++) nbuf[i] = buf.u8[i+buf_offset];
+        var real_fd = typeof fdo.fd === 'number' ? fdo.fd : fd;
+        if(typeof fdo.pos === 'number') fdo.pos += n;
+        h$fs.write(real_fd, nbuf, 0, n, pos, function(err, bytesWritten) {
+            ;
+            if(err) {
+                h$setErrno(err);
+                if(typeof fdo.pos === 'number') fdo.pos -= n;
+                if(h$errno === 35)
+                    setTimeout(function() { h$base_writeFile(fd, fdo, buf, buf_offset, n, c); }, 20);
+                else c(-1);
+            } else {
+                if(typeof fdo.pos === 'number') fdo.pos += bytesWritten - n;
+                c(bytesWritten);
+            }
+        });
+    }
+    h$base_writeStdout = function(fd, fdo, buf, buf_offset, n, c) {
+        ;
+        h$base_writeFile(1, fdo, buf, buf_offset, n, c);
+    }
+    h$base_closeStdout = function(fd, fdo, c) {
+        ;
+        c(0);
+    }
+    h$base_writeStderr = function(fd, fdo, buf, buf_offset, n, c) {
+        ;
+        h$base_writeFile(2, fdo, buf, buf_offset, n, c);
+    }
+    h$base_closeStderr = function(fd, fdo, c) {
+        ;
+        c(0);
+    }
+    process.stdin.on('readable', h$base_process_stdin);
+    process.stdin.on('end', function() { h$base_stdin_eof = true; h$base_process_stdin(); });
+    h$base_isattyStdin = function() { return process.stdin.isTTY; };
+    h$base_isattyStdout = function() { return process.stdout.isTTY; };
+    h$base_isattyStderr = function() { return process.stderr.isTTY; };
+} else if (h$isJsShell) {
+    h$base_readStdin = function(fd, fdo, buf, buf_offset, n, c) {
+        c(0);
+    }
+    h$base_writeStdout = function(fd, fdo, buf, buf_offset, n, c) {
+        putstr(h$decodeUtf8(buf, n, buf_offset));
+        c(n);
+    }
+    h$base_writeStderr = function(fd, fdo, buf, buf_offset, n, c) {
+        printErr(h$decodeUtf8(buf, n, buf_offset));
+        c(n);
+    }
+} else if(h$isJsCore) {
+    h$base_readStdin = function(fd, fdo, buf, buf_offset, n, c) {
+ c(0);
+    }
+    var h$base_stdoutLeftover = { f: print, val: null };
+    var h$base_stderrLeftover = { f: debug, val: null };
+    var h$base_writeWithLeftover = function(buf, n, buf_offset, c, lo) {
+ var lines = h$decodeUtf8(buf, n, buf_offset).split(/\r?\n/);
+ if(lines.length === 1) {
+     if(lines[0].length) {
+  if(lo.val !== null) lo.val += lines[0];
+  else lo.val = lines[0];
+     }
+ } else {
+            lo.f(((lo.val !== null) ? lo.val : '') + lines[0]);
+     for(var i=1;i<lines.length-1;i++) lo.f(lines[i]);
+     if(lines[lines.length-1].length) lo.val = lines[lines.length-1];
+     else lo.val = null;
+ }
+ c(n);
+    }
+    h$base_writeStdout = function(fd, fdo, buf, buf_offset, n, c) {
+ h$base_writeWithLeftover(buf, n, buf_offset, c, h$base_stdoutLeftover);
+    }
+    h$base_writeStderr = function(fd, fdo, buf, buf_offset, n, c) {
+ h$base_writeWithLeftover(buf, n, buf_offset, c, h$base_stderrLeftover);
+    }
+} else {
     h$base_readStdin = function(fd, fdo, buf, buf_offset, n, c) {
         c(0);
     }
@@ -3277,6 +3591,7 @@ var h$base_readFile, h$base_writeFile, h$base_closeFile;
         console.log(h$decodeUtf8(buf, n, buf_offset));
         c(n);
     }
+}
 var h$base_stdin_fd =
   { read: h$base_readStdin
   , close: h$base_closeStdin
