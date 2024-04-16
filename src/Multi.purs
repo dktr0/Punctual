d180775af82a172e4e095e780071285eea884bd8
dktr0 <ogbornd@mcmaster.ca>
2024-04-16 18:57:14 -0400

    new Multi approach tentatively reworked around a tighter model (just lists or lists of lists representing 'recent' combinatorial variation)

--- src/Multi.purs
+++ src/Multi.purs
@@ -1,17 +1,54 @@
 module Multi where
 
-import Prelude (class Functor,($),map,flip)
-import Data.List.NonEmpty (NonEmptyList)
+import Prelude (class Functor,($),map,flip,class Eq,(==),class Show,(<>),show,(<<<),class Apply,apply,class Applicative)
+import Data.List.NonEmpty (NonEmptyList,concat,singleton,cons,intercalate)
+import Control.Apply (lift2)
 
-data Multi a = Atom a | Multi (NonEmptyList (Multi a))
+import NonEmptyList (multi)
+import MultiMode (MultiMode(..))
+
+data Multi a = 
+  Multi1 (NonEmptyList a) | -- 1-dimensional, represents variants or channels
+  Multi2 (NonEmptyList (NonEmptyList a)) -- micro-dimension represents 'recent' variation
+  
+instance Eq a => Eq (Multi a) where
+  eq (Multi1 xs) (Multi1 ys) = xs == ys
+  eq (Multi2 xs) (Multi2 ys) = xs == ys
+  eq _ _ = false
+
+instance Show a => Show (Multi a) where
+  show (Multi1 xs) = "Multi1 (" <> show xs <> ")"
+  show (Multi2 xs) = "Multi2 (" <> show xs <> ")"
 
 instance Functor Multi where
-  map f (Atom a) = Atom (f a)
-  map f (Multi xs) = Multi $ map (map f) xs
- 
--- combinatorial: structure comes from first argument ("from the left"), unless it is an atom (no structure)
-atomAtomAtomC :: forall a b c. (a -> b -> c) -> Multi a -> Multi b -> Multi c
-atomAtomAtomC f (Atom a) b = map (f a) b
-atomAtomAtomC f a (Atom b) = map (flip f b) a
-atomAtomAtomC f (Multi a) b = Multi $ map (\a' -> atomAtomAtomC f a' b) a
+  map f (Multi1 xs) = Multi1 $ map f xs
+  map f (Multi2 xs) = Multi2 $ map (map f) xs
+
+instance Apply Multi where
+  apply (Multi1 fs) (Multi1 xs) = Multi2 $ map (\f -> map f xs) fs
+  apply fs xs = apply (Multi1 $ flatten fs) (Multi1 $ flatten xs) 
   
+flatten :: forall a. Multi a -> NonEmptyList a
+flatten (Multi1 xs) = xs
+flatten (Multi2 xs) = concat $ multi xs
+
+instance Applicative Multi where
+  pure x = Multi1 $ singleton x
+
+-- to be used, for example, in implementation of seq
+semiFlatten :: forall a. Multi a -> NonEmptyList (NonEmptyList a)
+semiFlatten (Multi1 xs) = singleton xs
+semiFlatten (Multi2 xs) = xs
+
+atomAtomAtom :: forall a b c. (a -> b -> c) -> Multi a -> Multi b -> Multi c
+atomAtomAtom = lift2
+
+test1 :: Multi Int
+test1 = Multi1 (1 `cons` (2 `cons` singleton 3))
+
+test2 :: Multi Int
+test2 = Multi1 (5 `cons` singleton 7)
+
+pp :: forall a. Show a => Multi a -> String
+pp (Multi1 xs) = "[" <> intercalate "," (map show xs) <> "]"
+pp (Multi2 xs) = "[" <> intercalate "," (map (pp <<< Multi1) xs) <> "]"
