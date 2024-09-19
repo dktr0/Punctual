7fbe3dc28ef92cd66ec03a98c845b72e7a4ee722
David Ogborn <ogbornd@mcmaster.ca>
2024-09-19 15:53:29 -0400

    0.5.0 (removed 0.4.x files)

--- library-src/Sound/Punctual/Duration.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE OverloadedStrings, DeriveGeneric, DeriveAnyClass #-}
-
-module Sound.Punctual.Duration where
-
-import GHC.Generics (Generic)
-import Control.DeepSeq
-
-data Duration = Seconds Rational | Cycles Rational deriving (Show,Eq,Generic,NFData)
