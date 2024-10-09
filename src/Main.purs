451995d8f93f9fdab028ccf5003f09df56e6d0cb
David Ogborn <ogbornd@mcmaster.ca>
2024-10-09 14:12:00 -0400

    more complete debug info in info pathway

--- src/Main.purs
+++ src/Main.purs
@@ -84,6 +84,8 @@ _newProgramInZone punctual zone newProgram = do
     case lookup zone programInfos of
       Just x -> pure x
       Nothing -> pure emptySignalInfo
+  let newProgramDebug = "new program: " <> show newProgram <> "\n\n"
+  let previousProgramDebug = "previous program: " <> show previousProgram <> "\n\n" 
   let newPrograms = insert zone newProgram programs
   let newPreviousPrograms = insert zone previousProgram previousPrograms
   let newProgramInfos = insert zone (programInfo newProgram) programInfos
@@ -94,8 +96,10 @@ _newProgramInZone punctual zone newProgram = do
   write newPreviousProgramInfos punctual.previousProgramInfos
   _updateCombinedProgramInfo punctual
   -- update visual rendering system
-  info <- case programHasVisualOutput newProgram of 
-    true -> updateWebGLForZone punctual zone newProgram previousProgram
+  fragShaderDebug <- case programHasVisualOutput newProgram of 
+    true -> do
+      fragShaderSrc <- updateWebGLForZone punctual zone newProgram previousProgram
+      pure $ "fragment shader: " <> fragShaderSrc <> "\n\n"
     false -> do
       deleteWebGLForZone punctual zone
       pure ""
@@ -103,7 +107,9 @@ _newProgramInZone punctual zone newProgram = do
   case programHasAudioOutput newProgram of
     true -> updateAudioForZone punctual zone newProgram
     false -> deleteAudioForZone punctual zone
-  pure info
+  let debugInfo = newProgramDebug <> previousProgramDebug <> fragShaderDebug
+  log debugInfo
+  pure debugInfo
       
 _updateCombinedProgramInfo :: Punctual -> Effect Unit
 _updateCombinedProgramInfo punctual = do
