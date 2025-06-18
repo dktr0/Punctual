d28b5e2abe6cd20c2badd2c545dcec0b75258047
David Ogborn <ogbornd@mcmaster.ca>
2025-06-18 16:46:21 -0400

    prototype of new approach without Signal type

--- src/NewSignal.purs
+++ src/NewSignal.purs
@@ -1,38 +1,62 @@
 module NewSignal where
 
-import W (W,Sample)
+import W (W)
 import G (G)
 import Matrix (Matrix)
-import Expr (Float)
 import AST (Expression)
+import Parsing (ParseError)
+import Data.Either (Either)
+import Action (Action)
+import Output (Output)
 
-type Signal = { audio :: W (Matrix Sample), video :: G (Matrix Float), expression :: Expression }
+-- Floats in Punctual might be constant/literal or variable/calculated
+-- and they might fall on either side of those lines differently on the audio-side than the video-side
+type Float = { audio :: W (Either Number String), video :: G (Either Number String) }
 
--- ??? type Signal m a = { gen :: m (Matrix a), expression :: Expression }
+type V a = Either ParseError a
 
-type SignalSignal = { signalSignal :: Signal -> Signal, expression :: Expression }
+newtype VariantFunction = VariantFunction (Variant -> V Variant)
+
+data Variant = 
+  String Expression String |
+  Int Expression Int | -- can also be used as Float and FloatMatrix
+  Float Expression Float | -- can also be used as FloatMatrix
+  FloatMatrix Expression (Matrix Float) |
+  Output Expression Output |
+  Action_v Expression Action |
+  VariantFunction_v Expression VariantFunction |
+  OutputOrVariantFunction Expression Output VariantFunction
 
 {-
+f x = x + 3 -- f :: VariantFunction 
 
-rmap :: SignalSignal -> Signal -> Signal
-rmap f x = { audio, video }
-  where
-    xs = divideIntoRows x -- :: NonEmptyList Signal
-	ys = map f.signalSignal xs -- :: NonEmptyList Signal
-	audio = do
-	  zs <- sequence $ map (_.audio) ys -- W (NonEmptyList ((Matrix Sample))
-	  pure ... put the rows in zs together as a single matrix
-	video = do
-	  zs <- sequence $ map (_.video) ys -- G (NonEmptyList ((Matrix Float)))
-	  pure ... put the rows in zs together as a single matrix
+rmap :: (FloatMatrix -> FloatMatrix) -> FloatMatrix -> FloatMatrix
 
-is this possible? divideIntoRows :: Signal -> NonEmptyList Signal
-yes, I think so!
-divideIntoRows x = 
-  ... getRows of x.audio by mapping :: W (NonEmptyList (NonEmptyList Sample))
-  ... getRows of x.video by mapping :: G (NonEmptyList (NonEmptyList Float))
-  ...zip the two sets of rows pairwise, turning each pair into a Signal
+rmap would check whether the provided first argument was a function
+that could accept a FloatMatrix as its first argument and produce a floatmatrix
+(possibly various special cases could be handled)
+because FloatMatrix is inherently/directly dividable into rows/channels, this will work
+and the former Signal type ceases to exist (essentially being replaced by FloatMatrix...)
 
-  -- x :: { audio :: W (Matrix Sample), video :: G (Matrix Float) }
+data Signal =
+  Constant Number |
+  SignalList MultiMode (List Signal) |
+  Append Signal Signal |
+  Zip Signal Signal |
+  Mono Signal |
+  Rep Int Signal |
+  Pi |
+  Px | Py | Pxy | Aspect |
+
+
+-- defined in Float.purs
+fx :: Float 
+fx = { audio: pure (Left 0.0), video: ...computation in W monad that accesses environment... }
+
+-- defined in Parser.purs
+parseReserved :: Expression -> String -> L Variant
+parseReserved e "fx" = pure $ Float e fx
+
+-- next step: build this out in separate modules, e.g. new Float module 
 
 -}
